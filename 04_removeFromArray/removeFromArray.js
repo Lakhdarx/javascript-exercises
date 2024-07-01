cae6c49c3c285222cc206e100253a775b80f057f
Lakhdar <Lakhdarbhl76@gmail.com>
2024-07-01 23:22:38 +0100

    Do exo4 with recursion

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...elements) {
+    for (let i of elements) {
+        let k = arr.indexOf(i);
+        if (k !== -1) arr.splice(k,1);
+        if (arr.indexOf(i) !== -1) removeFromArray(arr, i);
+    }
+    return arr;
 };
 
 // Do not edit below this line
