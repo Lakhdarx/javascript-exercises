cbdffbe0d567d26bc313d6b218b6011076f980be
Lakhdar <Lakhdarbhl76@gmail.com>
2024-07-01 12:44:50 +0100

    Do exo 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,11 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let wordReverse = '';
+    for (let i = word.length-1; i >=0; --i) {
+        wordReverse += word[i];
+    }
+    return wordReverse;
+    // A better way to do it is using JS built-in function
+    // return (word.split('').reverse().join(''));
 
 };
 
