919a7e3f2915d3aab8df2870d238888f3b33a8fe
Lakhdar <Lakhdarbhl76@gmail.com>
2024-07-08 22:18:28 +0100

    Do exo 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,20 @@
-const palindromes = function () {
+const palindromes = function (str) {
+
+    let cleanStr = str
+        .split('')
+        .filter((item => (item !== ',' && item !== '.' && item !== '!' && item !== ' ' && item !== ';') ))
+        .join('')
+        .toLowerCase();
+
+    let reverse = cleanStr
+        .split('')
+        .reverse()
+        .join('');
+    
+
+
+    if (reverse === cleanStr) return true;
+    else return false;
 
 };
 
