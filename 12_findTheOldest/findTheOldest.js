bb551f8b5009283b0b0b3097f8c934feb62e994c
Lakhdar <Lakhdarbhl76@gmail.com>
2024-07-09 11:21:54 +0100

    Do exo 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return (people.reduce((oldest, current) => {
+        let currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+        let oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        return currentAge > oldestAge ? current : oldest;
+    }));
 };
+
+
+
+
+
+
+function getAge(birthYear, deathYear) {
+    if (!deathYear) {
+        deathYear = new Date().getFullYear();
+    }
+    return deathYear - birthYear;
+}
+
+
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
